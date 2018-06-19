3250fcde409e82c387d6eeea840189a656473f0d
katejsenger@yahoo.com <katejsenger@yahoo.com>
2018-06-19 18:14:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// write your code below!
+function happyHolidays(string) 
+{
+  return "Happy holidays!"
+}
+
+function happyHolidayTo(string)
+{
+  return "Happy Holidays, ${Kate}!"
+}
+
+function happyHolidayTo(string)
+{
+  return "Happy ${Thanksgiving}, ${Kate}!"
+}
